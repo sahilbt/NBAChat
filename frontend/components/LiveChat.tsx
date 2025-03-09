e61d4a7e68a8b180ff046e09f3e69a0e74330274
Harshal Patel <patelharshal101@gmail.com>
2025-03-09 04:32:24 -0600

    initial frontend layout

---
+++ frontend/components/LiveChat.tsx
@@ -0,0 +1,9 @@
+const LiveChat = () =>{
+    return (
+        <div className="flex justify-center border border-black h-full w-[70%]">
+            <h1 className="text-4xl font-bold"> Live Chat </h1>
+        </div>
+    )
+}
+
+export default LiveChat;
