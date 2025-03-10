26e4ec878981f12d40937c2b9056e336a2de6110
Harshal Patel <patelharshal101@gmail.com>
2025-03-10 15:12:48 -0600

    Added socket connection to backend

--- frontend/src/app/components/LiveChat/LiveChat.tsx
+++ frontend/src/app/components/LiveChat/LiveChat.tsx
@@ -1,19 +1,79 @@
-import { SetStateAction, useState } from 'react';
+import { SetStateAction, useState, useEffect, useRef } from 'react';
 import { IoIosSend } from "react-icons/io";
 import messages from './messages.json';
 import Message from "./Message";
 
 const LiveChat = () =>{
+    const [nameValue, setNameValue] = useState('');
     const [inputValue, setInputValue] = useState('');
+    const [messages, setMessages] = useState<JSON[]>([]);
+    const sendSocketRef = useRef<WebSocket | null>(null); 
+    const receiveSocketRef = useRef<WebSocket | null>(null); 
+    const isClosingOrClosed = useRef(false); 
+
+    useEffect(() => {
+        // Socket to send messages
+        sendSocketRef.current = new WebSocket("ws://localhost:8000/messages/send-message");
+        sendSocketRef.current.onopen = () => {
+            console.log("Connected to WebSocket for sending messages");
+        };
+        sendSocketRef.current.onerror = (error) => {
+            console.error("WebSocket error for sending:", error);
+        };
+        sendSocketRef.current.onclose = () => {
+            console.log("WebSocket for sending messages closed");
+            isClosingOrClosed.current = true; 
+        };
+        
+        //Socket to recieve messages
+        receiveSocketRef.current = new WebSocket("ws://localhost:8000/messages/get-all-messages");
+        receiveSocketRef.current.onopen = () => {
+            console.log("Connected to WebSocket for receiving messages");
+        };
+        receiveSocketRef.current.onerror = (error) => {
+            console.error("WebSocket error for receiving:", error);
+        };
+        receiveSocketRef.current.onclose = () => {
+            console.log("WebSocket for receiving messages closed");
+        };
+        receiveSocketRef.current.onmessage = (event) => {
+            setMessages(JSON.parse(event.data)); 
+            console.log(messages)
+        };
+
+        return () => {
+            if (sendSocketRef.current && !isClosingOrClosed.current) {
+                sendSocketRef.current.close(); 
+                console.log("WebSocket for sending messages closed");
+            }
+            if (receiveSocketRef.current) {
+                receiveSocketRef.current.close(); 
+                console.log("WebSocket for receiving messages closed");
+            }
+        };
+    }, []); 
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(e.target.value);
-        console.log(inputValue)
-    }
+    };
+
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNameValue(e.target.value);
+    };
 
     const sendMessage = (e: React.MouseEvent) => {
-        console.log("Send Message")
-    }
+        if (sendSocketRef.current && sendSocketRef.current.readyState === WebSocket.OPEN && nameValue !== '') {
+            const json = {
+                user: nameValue, 
+                text: inputValue, 
+            };
+            sendSocketRef.current.send(JSON.stringify(json)); 
+            console.log("Sent message:", json);
+            setInputValue(""); 
+        } else {
+            console.error("WebSocket for sending is not open or initialized");
+        }
+    };
 
     return (
         <div className="border border-black h-full w-[70%] flex flex-col items-center">
@@ -23,10 +83,10 @@ const LiveChat = () =>{
 
             {/* chat area -> might need to make it a seperate component*/}
             <div className="border black h-[65%] w-[90%] pt-[1%] pl-[1%] mb-[4%] overflow-y-auto max-h-[65%]">
-                {messages.map((message) => (
+                {messages.map((message: any) => (
                     <Message 
-                        user={message.name}
-                        content={message.content}
+                        user={message.user}
+                        content={message.text}
                     />
                 ))}
             </div>
@@ -49,10 +109,15 @@ const LiveChat = () =>{
                         onClick={sendMessage}
                     />
                 </div>
-
             </div>
-
-
+            <div className="border border-black mb-[2%]">
+            <input 
+                className="border-none pl-[3%] pr-[3%] w-full " 
+                placeholder='Enter username here...'
+                value={nameValue}
+                onChange={handleNameChange}
+                />
+            </div>
         </div>
     )
 }
