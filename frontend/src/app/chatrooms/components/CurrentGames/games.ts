9e01719e140a8b37290b7c15c8ed8568de672317
Tommy Dinh <tommydinh@tommys-mbp-2.ucalgary.ca>
2025-03-13 15:03:05 -0600

    new games format for rendering teams

---
+++ frontend/src/app/chatrooms/components/CurrentGames/games.ts
@@ -0,0 +1,23 @@
+export const GAMES = [
+  {
+    id: 1,
+    homeTeam: "Portland Trailblazers",
+    homeTeamImg: "trailblazers.svg",
+    awayTeam: "Los Angeles Lakers",
+    awayTeamImg: "lakers.svg",
+  },
+  {
+    id: 2,
+    homeTeam: "Atlanta Hawks",
+    homeTeamImg: "hawks.svg",
+    awayTeam: "Sacramento Kings",
+    awayTeamImg: "kings.svg",
+  },
+  {
+    id: 3,
+    homeTeam: "Denver Nuggets",
+    homeTeamImg: "nuggets.svg",
+    awayTeam: "New York Knicks",
+    awayTeamImg: "knicks.svg",
+  },
+];
